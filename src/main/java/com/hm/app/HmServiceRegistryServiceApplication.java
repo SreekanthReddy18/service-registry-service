82677a905dda2b5c16880327ab8c7e790f2efa76
Sreekanth Reddy <sreekanth3143@gmail.com>
2021-07-28 08:29:52 +0530

    initial commit

---
+++ src/main/java/com/hm/app/HmServiceRegistryServiceApplication.java
@@ -0,0 +1,15 @@
+package com.hm.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class HmServiceRegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmServiceRegistryServiceApplication.class, args);
+	}
+
+}
